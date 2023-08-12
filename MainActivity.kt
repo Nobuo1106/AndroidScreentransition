d63cc0b67af7ab060162574077fc43fe6602eb8f
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-12 15:20:31 +0900

    done

--- MainActivity.kt
+++ MainActivity.kt
@@ -2,6 +2,8 @@ package com.websarva.wings.android.screentransition
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
@@ -9,33 +11,73 @@ import android.widget.SimpleAdapter
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var _menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+    private val _from = arrayOf("name", "price")
+    private val _to = intArrayOf(R.id.tvMenuNameRow, R.id.tvMenuPriceRow)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _menuList = createTeishokuList()
         val lvMenu = findViewById<ListView>(R.id.lvMenu)
-        val menuList: MutableList<MutableMap<String, String>> = mutableListOf()
-        var menu = mutableMapOf("name" to "ハンバーグ定食", "price" to "850円")
-        menuList.add(menu)
-        menu = mutableMapOf("name" to "から揚げ定食", "price" to "800円")
-        menuList.add(menu)
 
         val from = arrayOf("name", "price")
         val to = intArrayOf(android.R.id.text1, android.R.id.text2)
-        val adapter = SimpleAdapter(this@MainActivity, menuList, android.R.layout.simple_list_item_2, from , to)
+        val adapter = SimpleAdapter(this@MainActivity, _menuList, android.R.layout.simple_list_item_2, from , to)
         lvMenu.adapter = adapter
         lvMenu.onItemClickListener = ListItemClickListener()
     }
 
+    private fun createTeishokuList() : MutableList<MutableMap<String, Any>> {
+        val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+        var menu = mutableMapOf<String, Any>("name" to "から揚げ定食", "price" to 800, "desc" to "若鶏の唐揚げにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf<String, Any>("name" to "ハンバーグ定食", "price" to 900, "desc" to "てごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+
+        return menuList
+    }
+
+    private fun createCurryList(): MutableList<MutableMap<String, Any>> {
+        val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+        var menu = mutableMapOf<String, Any>("name" to "ビーフカレー", "price" to 520, "desc" to "特選スパイスをきかせた国産ビーフ100%のカレーです。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ポークカレー", "price" to 420, "desc" to "特選スパイスをきかせた国産ポーク100%のカレーです。")
+        menuList.add(menu)
+
+        return menuList
+    }
+
     private inner class ListItemClickListener : AdapterView.OnItemClickListener {
         override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id : Long) {
-            val item = parent.getItemAtPosition(position) as MutableMap<String, String>
-            val menuName = item["name"]
-            val menuPrice = item["price"]
+            val item = parent.getItemAtPosition(position) as MutableMap<String, Any>
+            val menuName = item["name"] as String
+            val menuPrice = item["price"] as Int
             val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
             intent2MenuThanks.putExtra("menuName", menuName)
-            intent2MenuThanks.putExtra("menuPrice", menuPrice)
+            intent2MenuThanks.putExtra("menuPrice", "${menuPrice}円")
             startActivity(intent2MenuThanks)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options_menu_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var returnVal = true
+        when(item.itemId) {
+            R.id.menuListOptionTeishoku ->
+                _menuList = createTeishokuList()
+            R.id.menuListOptionCurry ->
+                _menuList = createCurryList()
+            else ->
+                returnVal = super.onOptionsItemSelected(item)
+        }
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val adapter = SimpleAdapter(this@MainActivity, _menuList, R.layout.row, _from, _to)
+        lvMenu.adapter = adapter
+        return returnVal
     }
 }
